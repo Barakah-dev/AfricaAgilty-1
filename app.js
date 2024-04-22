df9382d7f2c8ffae48b15c0e99870be87351cb58
Barakat Akinrinde <barakatakinrinde@gmail.com>
2024-04-22 09:39:44 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+let wearsGlasses = true;
+const name = "Barakah";
+var shoeSize = 38;
+const height = 154;
+var weight = 50;
---
+++ script.js
@@ -0,0 +1,7 @@
+function profit(sellingPrice, costPrice) {
+  return sellingPrice - costPrice;
+}
+
+console.log(profit(30, 20));
+console.log(profit(50, 20));
+console.log(profit(100, 50));
